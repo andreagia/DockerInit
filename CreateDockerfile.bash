c10ee96af57e9e53a9fe899747054a43e4c07a11
andrea <andreagia71@gmail.com>
2017-04-13 11:41:30 +0200

    1

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -1,4 +1,14 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-get install -y docker-engine
+
+
 sed "s/TOKEN1/$1/g" "Dokerfile.tmp" > ./tmp && mv ./tmp Dokerfile
 sed "s/PROVIDER2/$2/g" "Dokerfile" > ./tmp && mv ./tmp Dokerfile
+
+docker build -t saas .
+docker run -p 8080:8080  --privileged --dns 150.217.1.32 saas
 
