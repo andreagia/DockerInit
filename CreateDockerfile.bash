9c6a1fb9aee74c85b85a8ee39f3a127d9cac17f0
andrea <andreagia71@gmail.com>
2017-04-14 17:16:01 +0200

    4

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -3,7 +3,7 @@ apt-get install apt-transport-https  ca-certificates  curl  software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y --allow-unauthenticated docker-engine
+apt-get install -y --allow-unauthenticated docker-ce
 
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
 sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
