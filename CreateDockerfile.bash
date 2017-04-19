52093e596eaf2e8bfb49198ce1d6b5c434741412
andrea <andreagia71@gmail.com>
2017-04-19 12:13:29 +0200

    7

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -3,11 +3,11 @@ apt-get install apt-transport-https  ca-certificates  curl  software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y --allow-unauthenticated docker-ce
+apt-get install -y docker-ce
 
 WANIP=$(curl http://checkip.amazonaws.com)
-openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca creates=./mds2.cert.pem
-ed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
+openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
+sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
 sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
