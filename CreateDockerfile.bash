1c70b109aa496e864f56e28df1153044f2a81c75
andrea <andreagia71@gmail.com>
2017-04-19 11:27:20 +0200

    6

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -5,6 +5,9 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $
 apt-get update
 apt-get install -y --allow-unauthenticated docker-ce
 
+WANIP=$(curl http://checkip.amazonaws.com)
+openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca creates=./mds2.cert.pem
+ed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
 sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
