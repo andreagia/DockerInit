9bb22b8601b211f14b21e452496d5c4309686780
andrea <andreagia71@gmail.com>
2017-04-26 11:50:59 +0200

    Reoved ngnix miltipart problems

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -6,12 +6,13 @@ apt-get update
 apt-get install -y docker-ce
 
 WANIP=$(curl http://checkip.amazonaws.com)
-openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
-sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
+keytool -genkey -alias jetty -keyalg RSA -keystore ./jetty.keystore -storepass secret -keypass secret -dname "CN=$WANIP"
+#openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
+#sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
 sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
 docker build -t saas .
-docker run -p 8080:8080 saas
-#docker run -p 443:443 saas
+#docker run -p 8080:8080 saas
+docker run -p 443:8443 saas
 
