aa264a024489e31f378e9949c674ace210934b81
andrea <andreagia71@gmail.com>
2017-04-27 09:48:09 +0200

    added variable to container

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y docker-ce
 
 WANIP=$(curl http://checkip.amazonaws.com)
-keytool -genkey -alias jetty -keyalg RSA -keystore ./jetty.keystore -storepass secret -keypass secret -dname "CN=$WANIP"
+#keytool -genkey -alias jetty -keyalg RSA -keystore ./jetty.keystore -storepass secret -keypass secret -dname "CN=$WANIP"
 #openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
 #sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
@@ -14,5 +14,5 @@ sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
 docker build -t saas .
 #docker run -p 8080:8080 saas
-docker run -p 443:8443 saas
+docker run -e SECHASH=$SECHASH -e WANIP=$WANIP -p 443:8443 saas
 
