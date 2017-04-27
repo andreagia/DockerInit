af1f8c30e74e5c22ad0684de4e6c8e1b7d49da86
andrea <andreagia71@gmail.com>
2017-04-27 10:44:30 +0200

    dcc

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -5,14 +5,16 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $
 apt-get update
 apt-get install -y docker-ce
 
-WANIP=$(curl http://checkip.amazonaws.com)
+export WANIP=$(curl http://checkip.amazonaws.com)
 #keytool -genkey -alias jetty -keyalg RSA -keystore ./jetty.keystore -storepass secret -keypass secret -dname "CN=$WANIP"
 #openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
 #sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
 sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+sed "s/wanip1/$WANIP/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+sed "s/sechash1/$SECHASH/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
-docker build --build-arg WANIP=$WANIP --build-arg SECHASH=$SECHASH -t saas .
+docker build -t saas .
 #docker run -p 8080:8080 saas
 docker run -p 443:8443 saas
 
