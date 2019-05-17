4a41b70a5b9b92a7158e202ff1571663539f889f
andrea <andreagia71@gmail.com>
2019-05-17 13:02:41 +0200

    added onadata

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -10,9 +10,9 @@ export WANIP=$(curl http://checkip.amazonaws.com)
 #openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
 #sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 #ONEDATA Confifuration removed
-#sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
-#sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
-sed "s/wanip1/$WANIP/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
+sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
+sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+sed "s/wanip1/$WANIP/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 sed "s/sechash1/$SECHASH/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
 
 docker build -t saas .
