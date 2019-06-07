560de740961f9d2b75fca9e9fd9a47931724854b
andrea <andreagia71@gmail.com>
2019-06-07 11:43:52 +0200

    Added check for onedata

--- CreateDockerfile.bash
+++ CreateDockerfile.bash
@@ -10,10 +10,19 @@ export WANIP=$(curl http://checkip.amazonaws.com)
 #openssl req -new -nodes -x509 -subj "/C=IT/ST=Oregon/L=Florence/O=IT/CN=$WANIP" -days 3650 -keyout ./mds2.key.pem -out ./mds2.cert.pem -extensions v3_ca 
 #sed "s/WANIP/$WANIP/g" "nginx.conf.tmp" > ./tmp && mv ./tmp nginx.conf
 #ONEDATA Confifuration removed
-sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
-sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
-sed "s/wanip1/$WANIP/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
-sed "s/sechash1/$SECHASH/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+TK_LENGTH=${#VTOKEN1}
+echo $TK_LENGTH
+if [ $TK_LENGTH -gt 5 ]; then
+    echo "TOKEN PRESENT"
+    sed "s/TOKEN1/$1/g" "Dockerfile.tmp" > ./tmp && mv ./tmp Dockerfile
+    sed "s/PROVIDER2/$2/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+    sed "s/wanip1/$WANIP/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+    sed "s/sechash1/$SECHASH/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+else
+    echo "TOKEN NOT PRESENT"
+    sed "s/wanip1/$WANIP/g" "Dockerfile.no" > ./tmp && mv ./tmp Dockerfile
+    sed "s/sechash1/$SECHASH/g" "Dockerfile" > ./tmp && mv ./tmp Dockerfile
+fi
 
 docker build -t saas .
 #docker run -p 8080:8080 saas
